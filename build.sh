2c2f7b1017fec27b3e6517cf602b23af2bfa4378
Gabriel-Augusto21 <gomides.augustog@gmail.com>
2026-04-09 21:16:39 -0300

    tentando deploy

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py ler
-python manage.py inserir_comp_exigencias
+python manage.py apagar_dados
 python manage.py inserir_dados
+python manage.py inserir_comp_exigencias
+
